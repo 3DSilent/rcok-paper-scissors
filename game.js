840217a82d6c7f6dde957dc0ed66663f578c6c94
3DSilent <232480925+3DSilent@users.noreply.github.com>
2025-09-25 00:47:18 +0100

    Converts the result of getComputerChoise to words

--- game.js
+++ game.js
@@ -1,4 +1,13 @@
 function getComputerChoice() {
     let val = Math.floor(Math.random() * 3);
-    return val;
+    let computerChoice = val === 0 ? "rock" :
+    val === 1 ? "paper" :
+    val === 2 ? "scissors" : null ;
+    return computerChoice;  
 }
+
+function getHumanChoice() {
+    let choice = prompt("Make your choice.", "rock, paper, scissors");
+    return humanChoice;
+}
+
